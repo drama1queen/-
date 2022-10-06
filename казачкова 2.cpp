6128e338eda033d260157576828151649fd388eb
drama1queen <114808862+drama1queen@users.noreply.github.com>
2022-10-06 22:08:14 +0400

    Add files via upload

---
+++ казачкова 2.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    double p, s;
+    cin >> x >> y >> z;
+    p = (x + y + z) / 2;
+    s = sqrt(p * (p - x) * (p - y) * (p - z));
+    cout << s << endl;
+}
+
